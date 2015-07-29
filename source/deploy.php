267dbac80e2e12b75a2b36bcbcc45018d199959d
Dmitry Burim <d.bourim@gmail.com>
2015-07-29 22:51:33 +0300

    upd

---
+++ source/deploy.php
@@ -0,0 +1,31 @@
+<?php
+
+$secret = file_get_contents('../.github_secret');
+
+// Split signature into algorithm and hash
+$headers      = getallheaders();
+$hub_signature = $headers['X-Hub-Signature'];
+list($algo, $hash) = explode('=', $hub_signature, 2);
+
+// Get payload
+$payload = file_get_contents('php://input');
+
+// Calculate hash based on payload and the secret
+$payload_hash = hash_hmac($algo, $payload, $secret);
+
+// Check if hashes are equivalent
+if ($hash !== $payload_hash) {
+    header('HTTP/1.0 401 Unauthorized');
+    die('401 Unauthorized');
+}
+
+chdir('../repo');
+
+shell_exec('hexo config render_drafts false');
+shell_exec('hexo config public');
+
+echo shell_exec('hexo generate -f');
+echo shell_exec('rm -rf ../public');
+echo shell_exec('rm -rf ../public_html.bak');
+echo shell_exec('mv ../public_html ../public_html.bak');
+echo shell_exec('mv public ../public_html');
